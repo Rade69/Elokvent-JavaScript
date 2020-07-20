cbf117fa574a0690269e67d1c6fc6de8ef6b3170
Rade69 <radovan1969@gmail.com>
2020-07-20 10:33:50 +0200

    Drugi nacin

--- chess-bord.js
+++ chess-bord.js
@@ -1,16 +1,28 @@
-let size = 8;
+// let size = 8;
 
-let board = "";
+// let board = "";
 
-for (let y = 0; y < size; y++) {
-  for (let x = 0; x < size; x++) {
-    if ((x + y) % 2 === 0) {
-      board += " ";
-    } else {
-      board += "#";
-    }
-  }
-  board += "\n";
+// for (let y = 0; y < size; y++) {
+//   for (let x = 0; x < size; x++) {
+//     if ((x + y) % 2 === 0) {
+//       board += " ";
+//     } else {
+//       board += "#";
+//     }
+//   }
+//   board += "\n";
+// }
+
+// console.log(board);
+
+var size = 8;
+var grid = "";
+
+for (i = 1; i <= size; i++) {
+  if (i % 2 === 0)
+    grid += "# ".repeat(size / 2) + "\n";
+  else
+    grid += " #".repeat(size / 2) + "\n";
 }
 
-console.log(board);
+console.log(grid);
